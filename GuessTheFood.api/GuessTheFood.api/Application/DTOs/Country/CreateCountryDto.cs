dd8716f375fd7efacaa879cd154a74650e9b80b6
Emerick <decdu59@live.fr>
2026-05-14 02:07:57 +0200

    feat: add dto for country

---
+++ GuessTheFood.api/GuessTheFood.api/Application/DTOs/Country/CreateCountryDto.cs
@@ -0,0 +1,6 @@
+namespace GuessTheFood.api.Application.DTOs.Country;
+
+public class CreateCountryDto
+{
+    public string Name { get; set; } = string.Empty;
+}
---
+++ GuessTheFood.api/GuessTheFood.api/Application/DTOs/Country/UpdateCountryDto.cs
@@ -0,0 +1,6 @@
+namespace GuessTheFood.api.Application.DTOs.Country;
+
+public class UpdateCountryDto
+{
+    public string Name { get; set; } = string.Empty;
+}
